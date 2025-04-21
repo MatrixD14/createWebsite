501c1a954d5859e90dea25aa7019cab90bfe55a9
MatrixD14 <deivisonjoelsouzacaldas@gmail.com>
2025-04-20 23:38:49 -0400

    update fire

--- id.js
+++ id.js
@@ -1,26 +1,41 @@
 const screenX = document.querySelector("#valorx"),
+  screenY = document.querySelector("#valory"),
   background = document.querySelector("#background"),
   frontSize = document.querySelector("#font_size"),
-  screenY = document.querySelector("#valory"),
   screenColor = document.querySelector("#Color"),
   screenName = document.querySelector("#name"),
-  editer = document.querySelector("#edite");
+  editer = document.querySelector("#edite"),
+  pai = document.querySelector("#stopbutton");
 let select = null;
 let armaze = [];
+screenName.value = "new type the object";
+frontSize.value = 10;
+screenX.value = 200;
+screenY.value = 50;
+background.value = "gray";
+screenColor.value = "white";
 buttonCreate("#par", "p");
 buttonCreate("#buttons", "button");
 buttonCreate("#block", "div");
 document.querySelector("#enter").addEventListener("click", function () {
   if (select) {
-    nomeObject(select);
+    nomeObject(
+      select,
+      screenName,
+      frontSize,
+      background,
+      screenColor,
+      screenX,
+      screenY
+    );
   }
 });
 
 function buttonCreate(name, typeObject) {
   document.querySelector(name).addEventListener("click", () => {
-    if (screenX.value > 0 && screenY.value > 0) {
+    if ((screenX.value > 0 && screenY.value > 0) || frontSize.value > 0) {
       create(typeObject);
-      noedita();
+      onoffedita("1", "auto");
       return;
     }
     messagem("add scale");
@@ -30,7 +45,7 @@ function buttonCreate(name, typeObject) {
 function logitaDelete2(arm) {
   document.querySelector("#delete").addEventListener("click", function () {
     if (select != null) {
-      noedita();
+      onoffedita("0", "none");
       select.remove();
       runlist = arm.indexOf(select);
       if (runlist !== -1) arm.splice(runlist, 1);
@@ -40,58 +55,106 @@ function logitaDelete2(arm) {
   });
 }
 
-function nomeObject(object) {
+function nomeObject(
+  object,
+  name,
+  fontsize,
+  background,
+  Color,
+  largura,
+  altura
+) {
   object.style.display = "flex";
   object.style.justifyContent = "center";
   object.style.alignItems = "center";
-  object.textContent = screenName.value;
-  object.style.fontSize = frontSize.value + "px";
-  object.style.backgroundColor = background.value;
-  object.style.color = screenColor.value;
-  object.style.width = screenX.value + "px";
-  object.style.height = screenY.value + "px";
+  object.textContent = name.value;
+  if (background?.value) object.style.backgroundColor = background.value;
+  if (Color?.value) object.style.color = Color.value;
+  if (!(largura?.value && largura.value > 0)) {
+    largura.value = 200;
+    object.style.width = largura.value + "px";
+    return;
+  }
+
+  if (!(altura?.value && altura.value > 0)) {
+    altura.value = 50;
+    object.style.height = altura.value + "px";
+    return;
+  }
+
+  if (!(fontsize?.value && fontsize.value > 0)) {
+    fontsize.value = 10;
+    object.style.fontSize = fontsize.value + "px";
+    return;
+  }
+  object.style.width = largura.value + "px";
+  object.style.height = altura.value + "px";
+  object.style.fontSize = fontsize.value + "px";
 }
 
 function create(html) {
   let object = document.createElement(html);
-  let onoff = true;
-  nomeObject(object);
+  let onoff = 0;
+  nomeObject(
+    object,
+    screenName,
+    frontSize,
+    background,
+    screenColor,
+    screenX,
+    screenY
+  );
   object.addEventListener("click", function (e) {
     e.stopPropagation();
     select = object;
-    onoff = !onoff;
+    onoff++;
     if (!armaze.includes(object)) {
       armaze.push(object);
       console.log(armaze.length);
     }
-    if (!onoff) {
-      edita();
+    if (onoff > 2) onoff = 0;
+    if (onoff == 2) {
+      onoffedita("1", "auto");
       object.style.outline = convertcolor(object);
       logitaDelete2(armaze);
-      screenName.value = select.textContent;
+      selectObject(object);
       armaze.forEach((el) => {
         if (el != object) el.style.outline = "none";
       });
       return;
     }
-    noedita();
-    selectObject();
+    onoffedita("0", "none");
+    deselectObject();
   });
+  moveobject(object, onoff);
   armaze.forEach((el) => {
     if (el !== object) el.style.outline = "none";
   });
-  document.querySelector("#stopbutton").appendChild(object);
+  pai.appendChild(object);
   return object;
 }
 
-function noedita() {
-  editer.style.opacity = "0";
-  editer.style.pointerEvents = "none";
+function onoffedita(opacitys, pointEvent) {
+  editer.style.opacity = opacitys;
+  editer.style.pointerEvents = pointEvent;
 }
 
-function edita() {
-  editer.style.opacity = "1";
-  editer.style.pointerEvents = "auto";
+function selectObject(object) {
+  screenName.value = object.textContent;
+  background.value = object.style.backgroundColor;
+  screenColor.value = object.style.color;
+  if ((screenX.value > 0 && screenY.value > 0) || frontSize.value > 0) {
+    frontSize.value = parseInt(object.style.fontSize) || "";
+    screenX.value = parseInt(object.style.width) || "";
+    screenY.value = parseInt(object.style.height) || "";
+  }
+}
+
+function deselectObject() {
+  select = null;
+  armaze.forEach((el) => {
+    el.style.outline = "none";
+  });
 }
 
 function convertcolor(object) {
@@ -107,26 +170,32 @@ function convertcolor(object) {
   console.log(colorarm);
   return colorarm;
 }
+function moveobject(object, onoff) {
+  if (!object) return;
+  let eixoObjX = 0,
+    eixoObjY = 0,
+    onoffmove = false;
+  object.style.position = "absolute";
+  object.addEventListener("mousedown", (e) => {
+    onoffmove = true;
+    eixoObjX = e.clientX - object.offsetLeft;
+    eixoObjY = e.clientY - object.offsetTop;
+  });
 
-function selectObject() {
-  select = null;
-  armaze.forEach((el) => {
-    el.style.outline = "none";
+  object.addEventListener("mousemove", (e) => {
+    if (!onoffmove || (onoff > 0 && onoff < 2)) return;
+    object.style.left = e.clientX - eixoObjX + "px";
+    object.style.top = e.clientY - eixoObjY + "px";
+  });
+  object.addEventListener("mouseup", () => {
+    onoffmove = false;
   });
 }
-
 function messagem(erro) {
   let object = document.createElement("p");
-  object.textContent = erro;
-  object.style.display = "flex";
-  object.style.justifyContent = "center";
-  object.style.alignItems = "center";
-  object.style.width = "300px";
-  object.style.height = "100px";
-  object.style.fontSize = "50px";
-  object.style.color = "white";
-  object.style.background = "gray";
-  document.querySelector("#stopbutton").appendChild(object);
+  nomeObject(object, erro, "50px", "gray", "white", "300px", "100px");
+  console.log("entro aqui");
+  pai.appendChild(object);
   setTimeout(() => {
     object.remove();
   }, 2000);
