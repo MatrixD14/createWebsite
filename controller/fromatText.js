bddc7293e85ed1e8dd442143f26b26f42d77157a
MatrixD14 <deivisonjoelsouzacaldas@gmail.com>
2025-04-27 10:01:25 -0400

    agora funciona a todas as tag

--- controller/fromatText.js
+++ controller/fromatText.js
@@ -1,12 +1,16 @@
 import { formatDualTag, formatOneTag } from "./variavels.js";
 export function fromattext(text) {
-  formatDualTag.forEach((tag) => {
-    let criatag = new RegExp(`\\{([^{}]+?)\\}\\s*${tag}(?!\\s*\\[)`, "gi");
-    text = text.replace(criatag, (_, conteudo) => {
-      return `&nbsp;<${tag}>${conteudo}</${tag}>&nbsp;`;
+  let specionemudansa;
+  do {
+    specionemudansa = false;
+    formatDualTag.forEach((tag) => {
+      let criatag = new RegExp(`\\{([^{}]+?)\\}\\s*${tag}(?!\\s*\\[)`, "gis");
+      text = text.replace(criatag, (_, conteudo) => {
+        specionemudansa = true;
+        return `&nbsp;<${tag}>${conteudo}</${tag}>&nbsp;`;
+      });
     });
-  });
-
+  } while (specionemudansa);
   formatOneTag.forEach((tag) => {
     let criatag = new RegExp(`\\/s*${tag}(?!\\s*\\[)`, "gi");
     text = text.replace(criatag, `<${tag}>&nbsp;`);
